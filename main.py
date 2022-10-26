b4633512def7c6ac9568eb749dac4f3288e9d105
kidus100 <kiyati.tech@gmail.com>
2022-10-26 19:08:22 -0400

    I have managed to make my code listen to messages sent to bot and send back the text

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import requests
 
-text = ""
 
 
-send = requests.get("https://api.telegram.org/bot5643026358:AAGyYJMh4pyXal1SQI0-6gV1bX2MF-pwamw/sendMessage?chat_id=395454426&text=hello")
 response = requests.get("https://api.telegram.org/bot5643026358:AAGyYJMh4pyXal1SQI0-6gV1bX2MF-pwamw/getUpdates")
-
 bot = response.json()
+length = len(bot["result"])
 
+while True:
+    response = requests.get("https://api.telegram.org/bot5643026358:AAGyYJMh4pyXal1SQI0-6gV1bX2MF-pwamw/getUpdates")
 
-print(bot)
+    bot = response.json()
+    recieved_text = bot["result"][len(bot["result"]) - 1]["message"]["text"]
 
-#
-# if text == "hi":
-#     #respond to the text
+    if len(bot["result"]) == length + 1:
+        length  =  length + 1
+        send_message = requests.get(f"https://api.telegram.org/bot5643026358:AAGyYJMh4pyXal1SQI0-6gV1bX2MF-pwamw/sendMessage?chat_id=395454426&text={recieved_text}")
 
 
