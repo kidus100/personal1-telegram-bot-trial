8e6e57fad9a17ba3ca0306124596e237f395a373
kidus100 <kiyati.tech@gmail.com>
2022-11-01 16:45:40 -0400

    bot used to send back message to me (kidus tilahun) even if the message sent was from a different person. but now the messsage sent back is from the person the message was recieved from.

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ while True:
     recieved_text = bot["result"][len(bot["result"]) - 1]["message"]["text"]
 
     if len(bot["result"]) == length + 1:
+        address_to_recieve_bot_message = bot["result"][len(bot["result"]) - 1]["message"]["from"]["id"]
         length  =  length + 1
-        send_message = requests.get(f"https://api.telegram.org/bot5643026358:AAGyYJMh4pyXal1SQI0-6gV1bX2MF-pwamw/sendMessage?chat_id=395454426&text={recieved_text}")
+        send_message = requests.get(f"https://api.telegram.org/bot5643026358:AAGyYJMh4pyXal1SQI0-6gV1bX2MF-pwamw/sendMessage?chat_id={address_to_recieve_bot_message}&text={recieved_text}")
 
 
